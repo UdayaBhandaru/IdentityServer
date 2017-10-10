37e99a705b6771432462cb39228b8006b5ab6716
UBhandaru <UBhandaru@INHYDUBHANDARU>
2017-10-10 15:06:39 +0530

    Startup Proj

--- IdentityServerPractice/Startup.cs
+++ IdentityServerPractice/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using IdentityServer;
+using Microsoft.Extensions.Logging;
 
 namespace IdentityServerPractice
 {
@@ -15,20 +17,34 @@ namespace IdentityServerPractice
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            ///This Code adds identity Server
+            services.AddIdentityServer()
+                    //Creates temporary key material at startup time. This is for dev only scenarios when you don’t have a certificate to use. The generated key will be persisted to the file system so it stays stable between server restarts (can be disabled by passing false). This addresses issues when the client/api metadata caches get out of sync during development
+                    .AddDeveloperSigningCredential()
+                    //Add Test Users
+                    .AddTestUsers(Config.GetUsers())
+                    // Adds in Memory Clients
+                    .AddInMemoryClients(Config.GetClients())
+                    // Add In Memroy Identity Resources
+                    .AddInMemoryIdentityResources(Config.GetIdentityResources());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILoggerFactory logger)
         {
+            logger.AddConsole();
+            logger.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseIdentityServer();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
         }
     }
 }
